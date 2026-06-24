c7e6e090b17762c5623bf3ee88ef7f264009dbe6
putraaa24 <trioputrakusuma29@gmail.com>
2026-06-25 02:19:02 +0700

    cors update

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import 'dotenv/config';
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://search-engine-fe.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: 'Content-Type,Authorization',
 };
