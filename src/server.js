ac6f497af9a492ed132f1776a064cb4daf3b1e07
putraaa24 <trioputrakusuma29@gmail.com>
2026-06-25 01:44:47 +0700

    cors update

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ import 'dotenv/config';
 
 const app = express();
 
-app.use(
-  cors({
-    methods: ['GET'],
-    origin: 'http://localhost:5173',
-  }),
-);
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const client = new Meilisearch({
